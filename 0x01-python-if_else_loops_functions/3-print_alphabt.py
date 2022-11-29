24857167c481cf15fc3f6c9564ff8512374be0f2
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-11-29 06:33:10 -0800

    edit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for i in 'abcdefghijklmnopqrstuvwxyz':
+    if (i != "q" and i != "e"):
+        print("{}".format(i), end="")

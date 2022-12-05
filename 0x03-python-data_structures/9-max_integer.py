2e6f7b654d64769ff2a782e75dedebe11e4f111f
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-12-05 00:56:54 -0800

    done

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
-
-    for i in list_keys:
-        new_dir[i] *= 2
-
-    return (new_dir) 
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_value = -9999
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    return max_value 

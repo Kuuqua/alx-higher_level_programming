4c208cff97fac5feac5a4541acab35a24c73cc8b
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-12-07 13:16:51 +0000

    semi

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-        return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)

f4109d98cb24d867665d82fc5d54ca7af7232905
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-12-07 13:14:45 +0000

    semi

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(my_dict):
-    for k in sorted(my_dict.keys()):
-            print("{}: {}".format(k, my_dict[k]))
+def print_sorted_dictionary(a_dictionary):
+    list_ord = list(a_dictionary.keys())
+    list_ord.sort()
+    for i in list_ord:
+        print("{}: {}".format(i, a_dictionary.get(i)))

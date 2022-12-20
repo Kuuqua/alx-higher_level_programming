ea3b19a43b01f154ce5cc251afacc97b3b3a7c23
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-12-20 15:42:39 +0000

    edit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+
+This module provides a simple Square class.
+"""
+
+
 class Square:
-    """Class Square that defines a square.
-    """
+    """A simple Square class"""
     pass

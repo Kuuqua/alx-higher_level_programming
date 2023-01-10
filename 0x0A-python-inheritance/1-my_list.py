28199a92baffb7dd73572a5618ffeb5e57f2dc39
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2023-01-10 07:21:32 +0000

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
+"""MyList module"""
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
-
+    """MyList class"""
     def print_sorted(self):
-        """prints the sorted list"""
-        print(sorted(self))
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
